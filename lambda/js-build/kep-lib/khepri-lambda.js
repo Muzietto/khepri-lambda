3f237c30e03b87f18b83b7c2c5eb35bd0512683d
Muzietto <marco_faustinelli@yahoo.com>
2015-04-20 08:45:48 +0200

    first lambda baby steps - commosso...

---
+++ lambda/js-build/kep-lib/khepri-lambda.js
@@ -0,0 +1,18 @@
+/*
+ * THIS FILE IS AUTO GENERATED FROM 'kep-source\kep-lib\khepri-lambda.kep'
+ * DO NOT EDIT
+*/
+define(["require", "exports"], (function(require, exports) {
+    "use strict";
+    var id, apply;
+    (id = (function(x) {
+        return x;
+    }));
+    (apply = (function(x) {
+        return (function(y) {
+            return x(y);
+        });
+    }));
+    (exports["id"] = id);
+    (exports["apply"] = apply);
+}));

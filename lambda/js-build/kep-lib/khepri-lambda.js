f5bf11754455e6a08e455c85724b3e079599570e
Muzietto <marco_faustinelli@yahoo.com>
2015-04-21 06:09:02 +0200

    attempting to add mocha & chai

--- lambda/js-build/kep-lib/khepri-lambda.js
+++ lambda/js-build/kep-lib/khepri-lambda.js
@@ -4,7 +4,7 @@
 */
 define(["require", "exports"], (function(require, exports) {
     "use strict";
-    var id, apply;
+    var id, apply, self_apply, identity2, first, second;
     (id = (function(x) {
         return x;
     }));
@@ -13,6 +13,26 @@ define(["require", "exports"], (function(require, exports) {
             return x(y);
         });
     }));
+    (self_apply = (function(s) {
+        return s(s);
+    }));
+    (identity2 = (function(x) {
+        return x;
+    }));
+    (first = (function(x) {
+        return (function(y) {
+            return x;
+        });
+    }));
+    (second = (function(x) {
+        return (function(y) {
+            return y;
+        });
+    }));
     (exports["id"] = id);
     (exports["apply"] = apply);
+    (exports["self_apply"] = self_apply);
+    (exports["identity2"] = identity2);
+    (exports["first"] = first);
+    (exports["second"] = second);
 }));
---
+++ lambda/spec-runner.js
@@ -0,0 +1,22 @@
+
+require.config({
+  'paths': {
+    'mocha': './js-build/js-lib/mocha',
+    'chai': './js-build/js-lib/chai',
+    'sinon': './js-build/js-lib/sinon-1.11.1'
+  },
+  shim: {
+    'mocha': {
+        init: function () {
+            this.mocha.setup('bdd');
+            return this.mocha;
+        }
+    }
+  }
+});
+
+define(['chai', 'mocha'], (function(chai, mocha) {
+    require(['./js-build/using_lambda'], function(require) {
+      mocha.run();
+    });
+}));

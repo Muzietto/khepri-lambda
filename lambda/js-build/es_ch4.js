23eced30b1d6a1b6f7a370621a480b8a7f7fc5f2
Muzietto <marco_faustinelli@yahoo.com>
2015-04-27 18:32:36 +0200

    starting ch4

---
+++ lambda/js-build/es_ch4.js
@@ -0,0 +1,13 @@
+/*
+ * THIS FILE IS AUTO GENERATED FROM 'kep-source\es_ch4.kep'
+ * DO NOT EDIT
+*/
+define(["require", "exports", "./lib/khepri-lambda"], (function(require, exports, lambda) {
+    "use strict";
+    var test4, _true = lambda["_true"],
+        _false = lambda["_false"],
+        not = lambda["not"];
+    (test4 = ({}));
+    (test4.not_true_is_false = (not(_true) === _false));
+    (exports["test4"] = test4);
+}));
--- lambda/test_lambda.js
+++ lambda/test_lambda.js
@@ -5,16 +5,34 @@ define([
   'chai', 
   './js-build/lib/khepri-lambda',
   './js-build/es_ch2',
-  './js-build/es_ch3'
-  ], (function(require, exports, chai, lambda, e2, e3) {
+  './js-build/es_ch3',
+  './js-build/es_ch4'
+  ], (function(require, exports, chai, lambda, e2, e3, e4) {
     "use strict";
     
   var expect = chai.expect,
     ch2suite = e2.test2,
-    ch3suite = e3.test3
+    ch3suite = e3.test3,
+    ch4suite = e4.test4
     ;
     
   describe('an implementation of lambda calculus in khepri',function(){
+
+    describe('includes chapter 4 stuff',function(){
+      for (var testcase in ch4suite){
+        (function(tc){
+          if (tc.indexOf('skip') === -1) {
+            it(tc,function(){
+                expect(ch4suite[tc]).to.be.ok;
+            });
+          } else {
+            it.skip(tc,function(){
+                expect(ch4suite[tc]).to.be.ok;
+            });            
+          }
+        })(testcase);
+      }
+    });
 
     describe('starts with chapter 2 definitions',function(){
       for (var testcase in ch2suite){
